7fd58b51d99dfa747dcbf4293f2c732bec39260b
carlaval655 <carla.val655@gmail.com>
2023-10-31 00:42:49 -0400

    Juego 1 Tercero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CuartoJuego2Component } from './components/cuarto-juego2/cuarto-juego2.component';
+import { TerceroJuego1Component } from './components/tercero-juego1/tercero-juego1.component';
 
 const routes: Routes = [
   { path: 'cuarto-juego2', component: CuartoJuego2Component },
+  { path: 'tercero-juego1', component: TerceroJuego1Component },
 ];
 
 @NgModule({
