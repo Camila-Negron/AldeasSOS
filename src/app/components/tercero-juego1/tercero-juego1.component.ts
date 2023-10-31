57de8d50232ff517996aea63b8ab4859e41412f4
carlaval655 <carla.val655@gmail.com>
2023-10-31 00:29:45 -0400

    Juego 1 tercero

---
+++ src/app/components/tercero-juego1/tercero-juego1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tercero-juego1',
+  templateUrl: './tercero-juego1.component.html',
+  styleUrls: ['./tercero-juego1.component.css']
+})
+export class TerceroJuego1Component {
+
+}
